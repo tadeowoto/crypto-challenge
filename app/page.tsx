cd9d3ca84913ee14060f1ae2212b5d39a833d566
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-17 09:32:57 -0300

    feat: html structure added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,37 @@
 export default function Home() {
   return (
-    <main>
-      <h1>Crypto challenge</h1>
-    </main>
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <header>
+          <h1>Top crypto coins</h1>
+          <p>
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda
+            officiis iste quaerat cum expedita amet quo incidunt. Exercitationem
+            cum minima ad odit doloremque optio expedita dignissimos eum.
+            Asperiores, accusamus placeat?
+          </p>
+        </header>
+        <section>
+          <h2>Coin List</h2>
+          <ul>
+            <li>Bitcoin</li>
+            <li>Ethereum</li>
+            <li>Litecoin</li>
+          </ul>
+        </section>
+      </main>
+    </>
   );
 }
