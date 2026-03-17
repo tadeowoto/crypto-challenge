7b4e55c206325e0b0f7e4e0c90c326d16ecd7278
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-17 09:54:42 -0300

    feat: initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,10 @@
 import { inter } from "./ui/fonts";
 import "./globals.css";
+import { Inter } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -7,7 +12,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
+    <html lang="es" className={cn("font-sans", inter.variable)}>
       <body
         className={`${inter.variable} antialiased bg-background-color text-text-color`}
       >
