19e83311dff8404bd0814f60ad574a117baff48f
Tadeo Wotoszyn <tadeowotoszyn@MacBook-Pro-de-Tadeo.local>
2026-03-18 19:51:10 -0300

    feat: error handler added

--- lib/api.ts
+++ lib/api.ts
@@ -1,11 +1,20 @@
 import { CoinMarket } from "@/types/coins";
 import { Coin } from "@/types/coins";
+import { ApiError } from "./exceptions";
 
 export const getCoinsById = async (id:string) => {
     
     const response = await fetch(`https://api.coingecko.com/api/v3/coins/${id}`);
     if (!response.ok) {
-    throw new Error("Something went wrong");
+
+
+      if(response.status == 404){ 
+        throw new ApiError("Coin not found", 404);
+      }
+      if (response.status === 429) {
+      throw new ApiError("Límite de peticiones alcanzado. Reintenta en un minuto.", 429);
+      }
+      throw new ApiError("Error al obtener los datos de la API.", response.status);
     }
 
     const data: CoinMarket = await response.json();
@@ -18,7 +27,14 @@ export const getListCoins = async () => {
   const response = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false')
 
   if (!response.ok) {
-    throw new Error("Something went wrong");
+
+    if(response.status == 404){ 
+      throw new ApiError("Coin not found", 404);
+    }
+    if (response.status === 429) {
+    throw new ApiError("Límite de peticiones alcanzado. Reintenta en un minuto.", 429);
+    }
+    throw new ApiError("Error al obtener los datos de la API.", response.status);
   }
 
   const data: Coin[] = await response.json();
---
+++ lib/exceptions.ts
@@ -0,0 +1,12 @@
+
+export class ApiError extends Error {
+    status: number;
+
+    constructor(message: string, status: number) {
+        super(message);
+        this.status = status;
+        Object.setPrototypeOf(this, ApiError.prototype);
+    }
+
+    
+}
